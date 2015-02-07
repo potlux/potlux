675fb8a05bed544973991c91c4d6e4e1adce036f
Aaron Rosen <azrosen92@gmail.com>
2015-02-06 22:01:07 -0500

    add crsf protection to image upload

--- potlux/views.py
+++ potlux/views.py
@@ -45,7 +45,7 @@ def new():
 		return render_template('submit.html')
 
 @app.route('/idea/<id>', methods=["GET", "POST"])
-# @login_required
+@login_required
 def show_idea(id):
 	idea = db.ideas.Idea.find_one({"_id" : ObjectId(id)})
 
