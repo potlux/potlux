58b4e06c3b9c14164093e208d855cfb2675eecaf
Aaron Rosen <azrosen92@gmail.com>
2015-06-29 00:45:05 -0400

    fix bug

--- potlux/views.py
+++ potlux/views.py
@@ -408,11 +408,13 @@ def register():
 def show_user(user_id):
 	user = db.users.User.find_one({'_id' : ObjectId(user_id)})
 	print user
-	favorites = db.ideas.Idea.find({
-		'_id' : {
-			'$in' : user.favorites
-		}
-	})
+	favorites = []
+	if user.favorites:
+		favorites = db.ideas.Idea.find({
+			'_id' : {
+				'$in' : user.favorites
+			}
+		})
 	print favorites
 	print "RENDERING TEMPLATE"
 	return render_template('profile.html', user=user, favorites=favorites)
